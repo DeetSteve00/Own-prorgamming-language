ccbd0b9c4b2a38f1ea34498fafad1427cb0bb9f1
DeetSteve00 <98794456+DeetSteve00@users.noreply.github.com>
2022-03-17 20:03:43 -0600

    Clean code

--- Interpreter/interpreter.h
+++ Interpreter/interpreter.h
@@ -1,56 +1,49 @@
 #ifndef INTERPRETER_H
 #define INTERPRETER_H
 
-FILE * program;
+SYNTAX_FUNCTION printOut(char line[]) {
+    int i = 0;
+    char out[300];
+    for(i = 0; line[i + 5] != STRING; i++) {
+        out[i] = line[i + 5];
+    }
+    out[i] = '\0';
+    if(line[4] != STRING) {
+		char varLocation[3];
+        for(int i = 4; line[i] != '\0'; i++) {
+            if(line[i] == (STRING || LINEBREAK)) SyntaxError(OUT);
+            varLocation[i - 4] = line[i];
+        }
+        int varLoc = atoi(varLocation);
+        printf("%d", varList[varLoc]);
+    } else printf("%s", out);
+}
+SYNTAX_FUNCTION inputVar(char line[]) {
+    char varLocation[3];
+    for(int i = 4; line[i] != '\0'; i++) {
+        if(line[i] == (STRING || LINEBREAK)) SyntaxError(OUT);
+        varLocation[i - 4] = line[i];
+    }
+    int varLoc = atoi(varLocation);
+    char input[1000];
+    gets(input);
+    varList[varLoc] = atoi(input);
+}
+SYNTAX_FUNCTION defineVar(char line[]) {
+    int varLength = 0;
+    char input[3];
+    for(int i = 4; line[i] != SPACE; i++) {
+        if(line[i] == (STRING || LINEBREAK)) SyntaxError(VAR);
+        input[i - 4] = line[i];
+        varLength++;
+    }
+    int varLocation = atoi(input);
+    for(int i = 5 + varLength; line[i] != '\0'; i++) {
+        if(line[i] == (STRING ||LINEBREAK)) SyntaxError(VAR);
+        input[i - 5 - varLength] = line[i];
+    }
+    int varValue = atoi(input);
+    varList[varLocation] = varValue;
+}
 
-INTERPRETER command(char code[]) {
-    char keyWord[4] = {code[0], code[1], code[2], '\0'};
-    if(strcmp(keyWord, "VAR") == 0) {
-        defineVar(code);
-    } else if(strcmp(keyWord, "INP") == 0) {
-        inputVar(code);   
-    } else if(strcmp(keyWord, "OUT") == 0) {
-        printOut(code);
-    } else if(strcmp(keyWord, "END") == 0) {
-        endProgram();
-    } else if(strcmp(keyWord, "NLN") == 0) {
-        printf("\n");
-    }
-    for(int i = 0; code[i] != '\0'; i++) if(code[i] == '!') return; // If comment is found, the line will end.
-}
-INTERPRETER readProgram(char file[]) {
-    program = fopen(file, "r");
-    char fileCode[500];
-	while(!feof(program)) {
-		fgets(fileCode, 500, program);
-		if(command(fileCode) > 0) SyntaxError(READ);
-	}
-    fclose(program);
-}
-INTERPRETER endProgram() {
-    getch();
-    exit(0);
-}
-INTERPRETER SyntaxError(int error) {
-    printf("ERROR: ");
-    switch(error) {
-        case OUT:
-            printf("Wrong OUT usage.\n");
-            break;
-        case VAR:
-            printf("Wrong VAR usage.\n");
-            break;
-        case INP:
-            printf("Wrong INP usage.\n");
-            break;
-        case READ:
-            printf("Something went wrong with the compiler\n");
-            break;
-        case NO_FUNCTION:
-            printf("Unknown function");
-            break;
-    }
-    getch();
-    exit(1);
-}
 #endif
--- Interpreter/main.c
+++ Interpreter/main.c
@@ -8,69 +8,78 @@
     #define LINEBREAK '\n' // '\n'
     #define SPACE 32 // ' '
 // PROGRAM
-    #define INTERPRETER int
+    #define COMPILER_FUNCTION int
     #define SYNTAX_FUNCTION void
-    INTERPRETER varList[300]; // Max vars = 300
-    INTERPRETER SyntaxError(int error);
+    #define SYNTAX_ERROR 1
+    #define NOERROR 0
+    int varList[300]; // Max vars = 300
 // FUNCTION MACROS
     #define OUT 1
     #define VAR 2
     #define INP 3
     #define READ 4
     #define NO_FUNCTION 5
-//
+// Compiler declarations
+    COMPILER_FUNCTION SyntaxError(int error);
+    COMPILER_FUNCTION endProgram();
+// ------------------------------------
 
+#include "interpreter.h"
 
-SYNTAX_FUNCTION printOut(char line[]) {
-    int i = 0;
-    char out[300];
-    for(i = 0; line[i + 5] != STRING; i++) {
-        out[i] = line[i + 5];
+COMPILER_FUNCTION command(char code[]) {
+    char keyWord[4] = {code[0], code[1], code[2], '\0'};
+    if(strcmp(keyWord, "VAR") == 0) {
+        defineVar(code);
+    } else if(strcmp(keyWord, "INP") == 0) {
+        inputVar(code);   
+    } else if(strcmp(keyWord, "OUT") == 0) {
+        printOut(code);
+    } else if(strcmp(keyWord, "END") == 0) {
+        endProgram();
+    } else if(strcmp(keyWord, "NLN") == 0) {
+        printf("\n");
     }
-    out[i] = '\0';
-    if(line[4] != STRING) {
-		char varLocation[3];
-        for(int i = 4; line[i] != '\0'; i++) {
-            if(line[i] == (STRING || LINEBREAK)) SyntaxError(OUT);
-            varLocation[i - 4] = line[i];
-        }
-        int varLoc = atoi(varLocation);
-        printf("%d", varList[varLoc]);
-    } else printf("%s", out);
+    for(int i = 0; code[i] != '\0'; i++) if(code[i] == '!') return NOERROR; // If comment is found, the line will end.
 }
-SYNTAX_FUNCTION inputVar(char line[]) {
-    char varLocation[3];
-    for(int i = 4; line[i] != '\0'; i++) {
-        if(line[i] == (STRING || LINEBREAK)) SyntaxError(OUT);
-        varLocation[i - 4] = line[i];
-    }
-    int varLoc = atoi(varLocation);
-    char input[1000];
-    gets(input);
-    varList[varLoc] = atoi(input);
+COMPILER_FUNCTION readProgram(char file[]) {
+    program = fopen(file, "r");
+    char fileCode[500];
+	while(!feof(program)) {
+		fgets(fileCode, 500, program);
+		if(command(fileCode) > 0) SyntaxError(READ);
+	}
+    fclose(program);
 }
-SYNTAX_FUNCTION defineVar(char line[]) {
-    int varLength = 0;
-    char input[3];
-    for(int i = 4; line[i] != SPACE; i++) {
-        if(line[i] == (STRING || LINEBREAK)) SyntaxError(VAR);
-        input[i - 4] = line[i];
-        varLength++;
-    }
-    int varLocation = atoi(input);
-    for(int i = 5 + varLength; line[i] != '\0'; i++) {
-        if(line[i] == (STRING ||LINEBREAK)) SyntaxError(VAR);
-        input[i - 5 - varLength] = line[i];
-    }
-    int varValue = atoi(input);
-    varList[varLocation] = varValue;
-}
-
 int main(int argc, char* argv[]) {
     if(argc < 1) return 1;
     readProgram(argv[1]);
     getch();
     return 0;
 }
-
-#include "interpreter.h"
+// ------------------------------------
+COMPILER_FUNCTION endProgram() {
+    getch();
+    exit(0);
+}
+COMPILER_FUNCTION SyntaxError(int error) {
+    printf("ERROR: ");
+    switch(error) {
+        case OUT:
+            printf("Wrong OUT usage.\n");
+            break;
+        case VAR:
+            printf("Wrong VAR usage.\n");
+            break;
+        case INP:
+            printf("Wrong INP usage.\n");
+            break;
+        case READ:
+            printf("Something went wrong with the compiler\n");
+            break;
+        case NO_FUNCTION:
+            printf("Unknown function");
+            break;
+    }
+    getch();
+    exit(1);
+}
